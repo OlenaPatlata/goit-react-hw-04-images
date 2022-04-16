08072d401e2876c8cb5d0e84b309f37d01518075
OlenaPatlata <94738131+OlenaPatlata@users.noreply.github.com>
2022-04-16 19:53:21 +0300

    add modal window

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
+import Modal from 'components/Modal/Modal';
 
 // import s from './App.module.css';
 
 class App extends Component {
-  state = {};
+  state = { showModal: false };
+
+  toggleModal = () => {
+    this.setState(({ showModal }) => ({ showModal: !showModal }));
+  };
 
   render() {
-    return <></>;
+    const { showModal } = this.state;
+    return (
+      <>
+        <button type="button" onClick={this.toggleModal}>
+          Открыть
+        </button>
+        {showModal && (
+          <Modal onClose={this.toggleModal}>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi
+              doloribus sint officiis expedita fugiat, libero iure asperiores
+              odio nulla neque cum commodi voluptates, voluptatibus ut rerum ea
+              explicabo dignissimos consectetur.
+            </p>
+            <button type="button" onClick={this.toggleModal}>
+              Close Modal
+            </button>
+          </Modal>
+        )}
+      </>
+    );
   }
 }
 export default App;
